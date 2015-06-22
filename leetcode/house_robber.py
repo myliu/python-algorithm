dc8906dc5852926f9a039cecf71820f6ebacd9d8
Mingyu Liu <mliu@quantcast.com>
2015-06-22 00:06:14 -0700

    House Robber

---
+++ leetcode/house_robber.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def rob(self, nums):
+        i = 0  # include the house
+        e = 0  # exclude the house
+        for n in nums:
+            # comma operator has to be used here because all the expressions 
+            # to the right of the assignment operator are evaluated before
+            # any of the assignments are made.
+            i, e = e + n, max(i, e)
+        return max(i, e)
+
+if __name__ == '__main__':
+    # Initialize Solution object
+    s = Solution()
+
+    nums = range(4)
+
+    print nums
+
+    print s.rob(nums)

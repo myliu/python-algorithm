a15c74fde80beec47f5b240da40fef1a7f78f5f5
Mingyu Liu <mliu@quantcast.com>
2015-12-20 17:05:41 -0800

    Generate parentheses

---
+++ leetcode/generate_parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        self.helper(result, '', n, n)
+        return result
+
+    def helper(self, result, current, left, right):
+        if left == 0 and right == 0:
+            result.append(current)
+            return
+
+        if left > 0:
+            self.helper(result, current + '(', left - 1, right)
+
+        if right > 0 and right > left:
+            self.helper(result, current + ')', left, right - 1)

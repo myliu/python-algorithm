7bcb797440bfd9dce06122ae7dd2267e550db404
Mingyu Liu <mliu@quantcast.com>
2016-01-10 08:26:05 -0800

    Add return value for union function - Number of Islands II

--- leetcode/number_of_islands_ii.py
+++ leetcode/number_of_islands_ii.py
@@ -1,4 +1,3 @@
-# Correct logic, but TLE
 class Solution(object):
     def numIslands2(self, m, n, positions):
         """
@@ -18,26 +17,31 @@ class Solution(object):
             return nodes[node]
         
         def union(node1, node2):
-            nodes[find(node1)] = find(node2)
+            x, y = find(node1), find(node2)
+            if x == y:
+                return 0
+            nodes[x] = y
+            return 1
 
         grid = [[0 for j in range(n)] for i in range(m)]
         nodes = {}
-        result = []
+        counts = []
+        count = 0
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         for pos in positions:
+            count += 1
             i, j = pos[0], pos[1]
             grid[i][j] = 1
             nodes[(i, j)] = (i, j)
             for d in directions:
                 x, y = i+d[0], j+d[1]
                 if valid(x, y, grid):
-                    union((i, j), (x, y))
-            count = len(set(map(find, nodes.keys())))
-            result.append(count)
+                    count -= union((i, j), (x, y))
+            counts.append(count)
 
-        return result
+        return counts
 
 if __name__ == '__main__':
     s = Solution()
-    # print s.numIslands2(3, 3, [[0,0],[0,1],[1,2],[2,1]])
-    print s.numIslands2(2, 2, [[0,0],[1,1],[0,1]])
+    print s.numIslands2(3, 3, [[0,0],[0,1],[1,2],[2,1]])
+    # print s.numIslands2(2, 2, [[0,0],[1,1],[0,1]])

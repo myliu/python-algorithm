4103076aa085e95e3e836826ec73c04d4fa7319e
Mingyu Liu <mliu@quantcast.com>
2016-01-10 14:25:01 -0800

    Two Sum II - Input array is sorted

---
+++ leetcode/two_sum_ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            sum = numbers[left] + numbers[right]
+            if sum == target:
+                return [left + 1, right + 1]
+            elif sum > target:
+                right -= 1
+            else:
+                left += 1

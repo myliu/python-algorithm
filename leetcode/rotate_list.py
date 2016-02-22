95a39e8034f3de00a77eb4180ce520bac2c2aa4a
Mingyu Liu <mliu@quantcast.com>
2016-02-21 22:28:10 -0800

    Rotate List

---
+++ leetcode/rotate_list.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+
+        current, count = head, 1
+        while current.next:
+            current = current.next
+            count += 1
+
+        # make a circle
+        current.next = head
+        
+        for i in range(count-k%count):
+            current = current.next
+
+        new_head = current.next
+        current.next = None
+        return new_head

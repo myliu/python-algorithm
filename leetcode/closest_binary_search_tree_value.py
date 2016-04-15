5f2b1bdbe717fdd8b974d3f0a1a2abfedde5026a
Mingyu Liu <mliu@quantcast.com>
2016-04-14 21:55:57 -0700

    Closest Binary Search Tree Value

---
+++ leetcode/closest_binary_search_tree_value.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def closestValue(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: float
+        :rtype: int
+        """
+        return self.helper(root, target, sys.maxint)
+
+    def helper(self, root, target, closest):
+        if root is None:
+            return closest
+
+        if target == root.val:
+            return root.val
+
+        closest = root.val if abs(root.val-target) < abs(closest-target) else closest
+
+        if target > root.val:
+            return self.helper(root.right, target, closest)
+
+        if target < root.val:
+            return self.helper(root.left, target, closest)

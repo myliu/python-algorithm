0bef9a5fcc755def010e1f0d98d813b613b6e355
Mingyu Liu <mliu@quantcast.com>
2016-07-31 16:12:04 -0700

    Recover Binary Search Tree

---
+++ leetcode/recover_binary_search_tree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+
+    def __init__(self):
+        self.first = None
+        self.second = None
+        self.prev = TreeNode(-float("inf"))
+
+
+    def recoverTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        self.traverse(root)
+        tmp = self.first.val
+        self.first.val = self.second.val
+        self.second.val = tmp
+
+
+    def traverse(self, root):
+        if not root:
+            return
+
+        self.traverse(root.left)
+
+        if not self.first and self.prev.val > root.val:
+            self.first = self.prev
+
+        if self.first and self.prev.val > root.val:
+            self.second = root
+
+        self.prev = root
+
+        self.traverse(root.right)

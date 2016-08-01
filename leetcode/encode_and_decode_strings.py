c2bbd8f88a63bc92d4b263a763d51f49c792dc4e
Mingyu Liu <mliu@quantcast.com>
2016-07-31 21:19:40 -0700

    Encode and Decode Strings

---
+++ leetcode/encode_and_decode_strings.py
@@ -0,0 +1,21 @@
+class Codec:
+
+    def encode(self, strs):
+        """Encodes a list of strings to a single string.
+        
+        :type strs: List[str]
+        :rtype: str
+        """
+        return ''.join((s.replace('|', '||') + ' | ' for s in strs))
+
+    def decode(self, s):
+        """Decodes a single string to a list of strings.
+        
+        :type s: str
+        :rtype: List[str]
+        """
+        return [s.replace('||', '|') for s in s.split(' | ')[:-1]]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))

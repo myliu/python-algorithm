e9cd6c23b30c69ee4a4c229f374c33a6750a304d
Mingyu Liu <mliu@quantcast.com>
2016-08-13 23:00:28 -0700

    Ransom Note

---
+++ leetcode/ransom_note.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        return False if Counter(ransomNote) - Counter(magazine) else True

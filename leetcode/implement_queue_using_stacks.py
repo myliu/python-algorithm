284ddcb4beaa40d3043cfa73379b2373b3639e20
Mingyu Liu <mliu@quantcast.com>
2016-08-15 16:44:02 -0700

    Implement Queue Using Stacks

---
+++ leetcode/implement_queue_using_stacks.py
@@ -0,0 +1,36 @@
+class Queue(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.input = []
+        self.output = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.input.append(x)
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        self.peek()
+        self.output.pop()
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not (self.input or self.output)

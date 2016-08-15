52717b6d00718ad5376dc8bccecb75ad80023062
Mingyu Liu <mliu@quantcast.com>
2016-08-14 23:02:12 -0700

    Power(x, n)

---
+++ leetcode/power(x,n).py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+
+        if n < 0:
+            n = -n
+            x = 1/x
+
+        return self.myPow(x*x, n/2) if n % 2 == 0 else x * self.myPow(x*x, n/2)

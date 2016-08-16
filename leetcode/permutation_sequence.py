48643ced1d71ca260c54824b26d283d3ecb4bcd2
Mingyu Liu <mliu@quantcast.com>
2016-08-16 10:06:45 -0700

    Permutation Sequence

---
+++ leetcode/permutation_sequence.py
@@ -0,0 +1,21 @@
+import math
+
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        nums = [str(i) for i in range(1, n+1)]
+        permutation = ''
+        # Convert from Ordinal to Cardinal number
+        k -= 1
+
+        # Iterate n times, and the length of 'permutation' increases by 1 in each iteration
+        while n > 0:
+            n -= 1
+            current, k = divmod(k, math.factorial(n))
+            permutation += nums[current]
+            nums.remove(nums[current])
+        return permutation

4033e348e39985c02284d731c568102f57f08e74
Mingyu Liu <mliu@quantcast.com>
2016-08-17 22:44:36 -0700

    Combinations - Version 1

---
+++ leetcode/combinations_v1.py
@@ -0,0 +1,10 @@
+from itertools import combinations
+
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        return list(combinations(range(1, n+1), k))

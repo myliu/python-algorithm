117430825fa4c2367086d13b6f36ed6b215d67ad
Mingyu Liu <mliu@quantcast.com>
2016-08-19 23:06:28 -0700

    Gray Code

---
+++ leetcode/gray_code.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        res = [0]
+        for i in range(n):
+            res += [2**i + x for x in reversed(res)]
+        return res

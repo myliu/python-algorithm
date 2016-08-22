c71bda5ee1ad621465985d72ea041aea2449ed77
Mingyu Liu <mliu@quantcast.com>
2016-08-22 00:25:54 -0700

    Valid Palindrome

---
+++ leetcode/valid_palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if not s:
+            return True
+
+        i, j = 0, len(s)-1
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+                continue
+            
+            if not s[j].isalnum():
+                j -= 1
+                continue
+
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+        return True

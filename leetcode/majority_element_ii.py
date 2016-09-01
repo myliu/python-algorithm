c322eb2332af72cb46a2f114190660d451b0c87f
Mingyu Liu <mliu@quantcast.com>
2016-08-31 17:17:46 -0700

    Majority Element II

---
+++ leetcode/majority_element_ii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        candidate1, candidate2, count1, count2 = 0, 1, 0, 0
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            else:
+                if count1 == 0:
+                    candidate1, count1 = num, 1
+                elif count2 == 0:
+                    candidate2, count2 = num, 1
+                else:
+                    count1 -= 1
+                    count2 -= 1
+        return [n for n in (candidate1, candidate2) if nums.count(n) > len(nums)/3]

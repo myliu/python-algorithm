7c32510aaeb01d7e899b3f8e32a4da3485dacb8f
Mingyu Liu <mliu@quantcast.com>
2016-09-25 16:50:57 -0700

    Russian Doll Envelopes - Version 2 (only check height)

--- leetcode/russian_doll_envelopes_v2.py
+++ leetcode/russian_doll_envelopes_v2.py
@@ -5,11 +5,11 @@ class Solution(object):
         :rtype: int
         """
         def compare(x, y):
-            if x[0] < y[0] or (x[0] == y[0] and x[1] < y[1]):
+            if x[0] < y[0] or (x[0] == y[0] and x[1] > y[1]):
                 return -1
             else:
                 return 1
-        
+
         if not envelopes:
             return 0
 
@@ -18,6 +18,6 @@ class Solution(object):
         liss = [1] * n
         for i in range(1, n):
             for j in range(i):
-                if envelopes[j][0] < envelopes[i][0] and envelopes[j][1] < envelopes[i][1]:
+                if envelopes[j][1] < envelopes[i][1]:
                     liss[i] = max(liss[i], liss[j]+1)
         return max(liss)

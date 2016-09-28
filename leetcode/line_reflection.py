6da7d5df8e8d2d1653faf6a5cd9b131370a7513d
Mingyu Liu <mliu@quantcast.com>
2016-09-27 22:26:03 -0700

    Line Reflection

---
+++ leetcode/line_reflection.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isReflected(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: bool
+        """
+        if not points:
+            return True
+
+        X = min(points)[0] + max(points)[0]
+        return {(x, y) for x, y in points} == {(X-x, y) for x, y in points}

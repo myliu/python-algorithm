481e130f112bc0ce4c2f16a6e2ac192da72d4918
Mingyu Liu <mliu@quantcast.com>
2016-10-01 21:43:02 -0700

    Insert Delete GetRandom O(1)

---
+++ leetcode/insert_delete_getrandom.py
@@ -0,0 +1,51 @@
+import random
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums = []
+        self.pos = {}
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.pos:
+            self.nums.append(val)
+            self.pos[val] = len(self.nums) - 1
+            return True
+        return False
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.pos:
+            idx, last = self.pos[val], self.nums[-1]
+            self.pos[last], self.nums[idx] = idx, last
+            self.nums.pop()
+            self.pos.pop(val)
+            return True
+        return False
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        # randint: Return a random integer N such that a <= N <= b.
+        return self.nums[random.randint(0, len(self.nums)-1)]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()

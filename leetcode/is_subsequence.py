6bacc73da8936ce77e843e4c3c949570fc06b541
Mingyu Liu <mliu@quantcast.com>
2016-10-04 11:10:06 -0700

    Is Subsequence

---
+++ leetcode/is_subsequence.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        t = iter(t)
+        for c in s:
+            if c not in t:
+                return False
+        return True

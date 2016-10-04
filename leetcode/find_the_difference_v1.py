ca5ccdbc69a9308477e1994e2c5294e7b19efcbe
Mingyu Liu <mliu@quantcast.com>
2016-10-03 21:56:59 -0700

    Find the Difference - Version 1

---
+++ leetcode/find_the_difference_v1.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        return (Counter(t) - Counter(s)).keys()[0]

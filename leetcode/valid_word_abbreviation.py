c280c98e3a29c52cfeff771750dc01e8c522edb4
Mingyu Liu <mliu@quantcast.com>
2016-10-09 22:54:42 -0700

    Valid Word Abbreviation

---
+++ leetcode/valid_word_abbreviation.py
@@ -0,0 +1,10 @@
+import re
+
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        """
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """
+        return bool(re.match(re.sub(r'([1-9]\d*)', r'.{\1}', abbr) + '$', word))

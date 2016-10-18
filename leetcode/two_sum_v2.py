3b5bf2bdeb0c4b1d261a8b9e2ddbc84e1c4470b4
Mingyu Liu <mliu@quantcast.com>
2016-10-18 16:31:04 -0700

    Two Sum - Version 2

---
+++ leetcode/two_sum_v2.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i, num in enumerate(nums):
+            if target-num in d:
+                return [i, d[target-num]]
+            d[num] = i

5cb20c4ba2a1f9175559c6ff980d179c4e337c5e
Mingyu Liu <mliu@quantcast.com>
2016-10-18 23:08:24 -0700

    Update Valid Number

--- leetcode/valid_number.py
+++ leetcode/valid_number.py
@@ -1,9 +1,21 @@
+import re
+
 class Solution(object):
-    regex = re.compile(r'^[+-]?((\d*\.\d+)|(\d+(\.\d*)?))([eE][+-]?\d+)?$')
+
+    def __init__(self):
+        self.pattern = re.compile(r'^[+-]?((\.\d+)|(\d+(\.\d*)?))([Ee][+-]?\d+)?$')
 
     def isNumber(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        return bool(self.regex.match(s.strip()))
+        return bool(self.pattern.match(s.strip()))
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.isNumber('+1')
+    print s.isNumber('.123')
+    print s.isNumber('0.123')
+    print s.isNumber('123')
+    print s.isNumber('e6')

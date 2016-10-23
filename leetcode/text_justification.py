6189f94d45a844046a113bdeef938d69036fe6db
Mingyu Liu <mliu@quantcast.com>
2016-10-23 00:28:24 -0700

    Update Text Justification

--- leetcode/text_justification.py
+++ leetcode/text_justification.py
@@ -5,17 +5,20 @@ class Solution(object):
         :type maxWidth: int
         :rtype: List[str]
         """
-        # res: list holding the final result
-        # cur: list holding the current line result
+        # results: list holding the final result
+        # current: list holding the current line result
         # length: total number of characters on the current line
-        res, cur, length = [], [], 0
-        for w in words:
-            if length + len(cur) + len(w) > maxWidth:
+        length, current, results = 0, [], []
+        for word in words:
+            if length + len(current) + len(word) > maxWidth:
                 for i in range(maxWidth - length):
-                    cur[i % (len(cur)-1 or 1)] += ' '
-                res.append(''.join(cur))
-                cur, length = [], 0
-            cur.append(w)
-            length += len(w)
-        res.append(' '.join(cur).ljust(maxWidth))
-        return res
+                    # or 1 is to deal with the case where len(current) is 1
+                    current[i % (len(current) - 1 or 1)] += ' '
+                results += ''.join(current),
+                length, current = 0, []
+            length += len(word)
+            current += word,
+
+        # Deal with the last line
+        results += ' '.join(current).ljust(maxWidth),
+        return results

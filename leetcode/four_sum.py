7c4c0ea6233a838133bfcac9c384bdd4f0d08a73
Mingyu Liu <mliu@quantcast.com>
2016-10-27 21:50:26 -0700

    Update 4Sum

--- leetcode/four_sum.py
+++ leetcode/four_sum.py
@@ -1,4 +1,5 @@
-import itertools, collections
+from itertools import combinations
+from collections import defaultdict
 
 class Solution(object):
     def fourSum(self, nums, target):
@@ -9,9 +10,10 @@ class Solution(object):
         """
         res = set()
 
-        two_sum = collections.defaultdict(list)
-        for (i1, n1), (i2, n2) in itertools.combinations(enumerate(nums), 2):
-            two_sum[n1+n2].append({i1, i2})
+        two_sum = defaultdict(list)
+        for (i1, n1), (i2, n2) in combinations(enumerate(nums), 2):
+            # Set is used because we will test disjoint and perform set union later.
+            two_sum[n1+n2] += {i1, i2},
 
         for t in two_sum.keys():
             if not two_sum[target-t]:
@@ -20,6 +22,11 @@ class Solution(object):
             for pair1 in two_sum[t]:
                 for pair2 in two_sum[target-t]:
                     if pair1.isdisjoint(pair2):
-                        res.add(tuple(sorted((nums[i] for i in pair1|pair2))))
+                        res.add(tuple(sorted(nums[i] for i in pair1|pair2)))
 
         return list(map(list, res))
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [1, 0, -1, 0, -2, 2]
+    print s.fourSum(nums, 0)

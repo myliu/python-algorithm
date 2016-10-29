36672cc313237da04774ba632adb7279d99ddadc
Mingyu Liu <mliu@quantcast.com>
2016-10-29 16:01:03 -0700

    Update Merge k Sorted Lists

--- leetcode/merge_k_sorted_lists.py
+++ leetcode/merge_k_sorted_lists.py
@@ -1,10 +1,10 @@
-import heapq
-
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+
+import heapq
 
 class Solution(object):
     def mergeKLists(self, lists):
@@ -12,13 +12,18 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        heap, res = [], []
-        for lst in lists:
-            while lst:
-                heapq.heappush(heap, lst.val)
-                lst = lst.next
+        heap = []
+        for head in lists:
+            if head:
+                heapq.heappush(heap, (head.val, head))
 
+        prev = dummy = ListNode(-1)
         while heap:
-            res.append(heapq.heappop(heap))
+            _, curr = heapq.heappop(heap)
+            _next = curr.next
+            if _next:
+                heapq.heappush(heap, (_next.val, _next))
+            prev.next = curr
+            prev = curr
 
-        return res
+        return dummy.next

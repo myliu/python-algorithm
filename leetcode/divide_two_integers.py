5f16934199c73595d4101fb3510b2e3a1b461247
Mingyu Liu <mliu@quantcast.com>
2016-10-30 22:58:30 -0700

    Divide Two Integers

--- leetcode/divide_two_integers.py
+++ leetcode/divide_two_integers.py
@@ -5,18 +5,22 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
-        positive = (dividend > 0) is (divisor > 0)
+        positive = (dividend > 0) == (divisor > 0)
         dividend, divisor = abs(dividend), abs(divisor)
-        res = 0
+        result = 0
         while dividend >= divisor:
-            i, temp = 1, divisor
-            while dividend >= temp:
-                dividend -= temp
-                res += i
-                temp <<= 1
+            i, tmp = 1, divisor
+            while dividend >= tmp:
+                dividend -= tmp
+                result += i
                 i <<= 1
+                tmp <<= 1
 
         if positive:
-            return min(2**31-1, res)
+            return min(2**31-1, result)
         else:
-            return max(-2**31, -res)
+            return max(-2**31, -result)
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.divide(15, 3)

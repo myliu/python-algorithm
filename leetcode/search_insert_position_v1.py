e2dc94db18ece9e61aea24e5e01854a74fc099ec
Mingyu Liu <mliu@quantcast.com>
2016-11-05 21:41:54 -0700

    Search Insert Position - Version 1

---
+++ leetcode/search_insert_position_v1.py
@@ -0,0 +1,10 @@
+from bisect import bisect_left
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return bisect_left(nums, target)

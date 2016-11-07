9f1caf0c003241c4e093dc4e19137e3de38b9a4e
Mingyu Liu <mliu@quantcast.com>
2016-11-06 23:34:10 -0800

    Update Multiple Strings

--- leetcode/multiple_strings.py
+++ leetcode/multiple_strings.py
@@ -5,10 +5,10 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        l1 = map(int, num1)[::-1]
-        l2 = map(int, num2)[::-1]
-        sum = 0
-        for i, m in enumerate(l1):
-            for j, n in enumerate(l2):
-                sum += m * n * 10 ** (i + j)
-        return str(sum)
+        lst1 = map(int, num1)[::-1]
+        lst2 = map(int, num2)[::-1]
+        _sum = 0
+        for i, m in enumerate(lst1):
+            for j, n in enumerate(lst2):
+                _sum += m * n * 10 ** (i+j)
+        return str(_sum)

f285e04bdddb2545118120788ad709f968c25c9d
Mingyu Liu <mliu@quantcast.com>
2016-11-07 10:24:31 -0800

    Update First Missing Positive

--- leetcode/first_missing_positive.py
+++ leetcode/first_missing_positive.py
@@ -7,6 +7,7 @@ class Solution(object):
         n = len(nums)
 
         for i in range(n):
+            # nums[i] != nums[nums[i]-1] cannot be nums[i] != i + 1 due to the case [1, 1]
             while nums[i] > 0 and nums[i] <= n and nums[i] != nums[nums[i]-1]:
                 j = nums[i] - 1
                 nums[i], nums[j] = nums[j], nums[i]

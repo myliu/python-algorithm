2bbd6ff99f332bd3ca1e862c2323305a0d5ea096
Mingyu Liu <mliu@quantcast.com>
2016-11-07 14:55:35 -0800

    Update Wildcard Matching - Version 2

--- leetcode/wildcard_matching_v2.py
+++ leetcode/wildcard_matching_v2.py
@@ -1,25 +1,36 @@
 class Solution(object):
 
+    """
+        ''  a   *   -> j(p)
+    ''  T   F   F
+    a   F   T   T
+    b   F   F   T
+    c   F   F   T
+
+    i(s)
+    """
     def isMatch(self, s, p):
         """
         :type s: str
         :type p: str
         :rtype: bool
         """
-        if len(p) - p.count('*') > len(s):
+        m, n = len(s), len(p)
+
+        if n - p.count('*') > m:
             return False
 
-        dp = [[False for j in range(len(p)+1)] for i in range(len(s)+1)]
+        dp = [[False] * (n+1) for _ in range(m+1)]
         dp[0][0] = True
 
-        for j in xrange(len(p)):
-            dp[0][j+1] = dp[0][j] and p[j] == '*'
+        for i in range(n):
+            dp[0][i+1] = dp[0][i] and p[i] == '*'
 
-        for j in xrange(len(p)):
-            for i in xrange(len(s)):
-                if p[j] != '*':
-                    dp[i+1][j+1] = dp[i][j] and (p[j] == s[i] or p[j] == '?')
+        for i in range(m):
+            for j in range(n):
+                if p[j] == '*':
+                    dp[i+1][j+1] = dp[i+1][j] or dp[i][j+1]
                 else:
-                    dp[i+1][j+1] = dp[i][j+1] or dp[i+1][j]
+                    dp[i+1][j+1] = dp[i][j] and p[j] in (s[i], '?')
 
-        return dp[len(s)][len(p)]
+        return dp[m][n]

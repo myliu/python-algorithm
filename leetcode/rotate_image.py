6bb883acc37f3af5646406c6d7f1eac1eee5925e
Mingyu Liu <mliu@quantcast.com>
2016-11-08 10:21:33 -0800

    Update Rotate Image

--- leetcode/rotate_image.py
+++ leetcode/rotate_image.py
@@ -1,10 +1,15 @@
 class Solution(object):
     """
-     * clockwise rotate
-     * first reverse up to down, then swap the symmetry 
+     * Clockwise rotate
+     * First reverse up to down, then swap the symmetry 
      * 1 2 3     7 8 9     7 4 1
      * 4 5 6  => 4 5 6  => 8 5 2
      * 7 8 9     1 2 3     9 6 3
+     *
+     * 1  2  3  4        13 14 15 16        13 9  5  1
+     * 5  6  7  8   =>   9  10 11 12   =>   14 10 6  2
+     * 9  10 11 12       5  6  7  8         15 11 7  3
+     * 13 14 15 16       1  2  3  4         16 12 8  4
     """
     def rotate(self, matrix):
         """

a45bab4817d1577e3ad9b54d33ab02c7998e8dfa
Mingyu Liu <mliu@quantcast.com>
2016-11-07 21:36:52 -0800

    Update Jump Game

--- leetcode/jump_game.py
+++ leetcode/jump_game.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        # Iterate over nums and update the maximal index that can be reached.
         reach = 0
         for k, v in enumerate(nums):
             if k > reach:

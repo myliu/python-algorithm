10723d315bc3c4f34ddebf8ab85c9223bdd6199e
Mingyu Liu <mliu@quantcast.com>
2016-11-09 14:18:08 -0800

    Update N-Queens II

--- leetcode/n_queens_ii.py
+++ leetcode/n_queens_ii.py
@@ -4,25 +4,29 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        def totalNQueensHelper(col, count):
-            for row in xrange(n):
-                if (row in rows) or (row + col in sum_diagonal) or (row - col in subtract_diagonal):
-                    continue
+        def validate(row, col):
+            if (row in rows) or (row+col in sum_diagonal) or (row-col in subtract_diagonal):
+                return False
+            return True
 
+        def dfs(col, count):
+            for row in range(n):
+                if not validate(row, col):
+                    continue
                 if col == n - 1:
                     count += 1
                 else:
                     rows.add(row)
                     sum_diagonal.add(row+col)
                     subtract_diagonal.add(row-col)
-                    count = totalNQueensHelper(col+1, count)
+                    count = dfs(col+1, count)
                     rows.remove(row)
                     sum_diagonal.remove(row+col)
                     subtract_diagonal.remove(row-col)
-
             return count
+
 
         rows = set()
         sum_diagonal = set()
         subtract_diagonal = set()
-        return totalNQueensHelper(0, 0)
+        return dfs(0, 0)

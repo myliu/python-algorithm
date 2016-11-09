6e72fc553a6650c54d39a9b783a46a437e4d04c4
Mingyu Liu <mliu@quantcast.com>
2016-11-09 10:56:36 -0800

    Update N-Queens

--- leetcode/n_queens.py
+++ leetcode/n_queens.py
@@ -5,29 +5,28 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         def validate(queens, x, y):
-            for i in xrange(n):
+            for i in range(n):
                 # No need to validate any column that equals to or greater than the current column
-                for j in xrange(y):
+                for j in range(y):
                     if queens[i][j] == 'Q' and (i == x or i + j == x + y or i - j == x - y):
                         return False
             return True   
 
+        # Invariant: All columns before col should satisfy the N-Queens rule.
         def dfs(queens, col, result):
             if col == n:
-                result.append(transform(queens))
+                result += [''.join(row) for row in queens],
                 return
 
-            # Iterate over the rows
-            for i in xrange(n):
-                if validate(queens, i, col):
-                    queens[i][col] = 'Q'
+            # Iterate over the rows, and try to place 'Q' on each row.
+            for row in range(n):
+                if validate(queens, row, col):
+                    queens[row][col] = 'Q'
                     dfs(queens, col+1, result)
-                    queens[i][col] = '.'
+                    queens[row][col] = '.'
 
-        def transform(queens):
-            return [''.join(row) for row in queens]
 
-        queens = [['.' for i in xrange(n)] for j in xrange(n)]
+        queens = [['.'] * n for _ in range(n)]
         result = []
         dfs(queens, 0, result)
         return result

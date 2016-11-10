4b548bfc67c1533ca87f2a0c79d82cf592e2b8a0
Mingyu Liu <mliu@quantcast.com>
2016-11-09 22:28:13 -0800

    Update Spiral Matrix

--- leetcode/spiral_matrix.py
+++ leetcode/spiral_matrix.py
@@ -4,28 +4,31 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        res = []
+        result = []
 
         if not matrix:
-            return res
+            return result
 
         row_begin, row_end, col_begin, col_end = 0, len(matrix)-1, 0, len(matrix[0])-1
+
         while row_begin <= row_end and col_begin <= col_end:
             for i in range(col_begin, col_end+1):
-                res.append(matrix[row_begin][i])
+                result += matrix[row_begin][i],
             row_begin += 1
 
             for i in range(row_begin, row_end+1):
-                res.append(matrix[i][col_end])
+                result += matrix[i][col_end],
             col_end -= 1
 
             if row_begin <= row_end:
                 for i in range(col_end, col_begin-1, -1):
-                    res.append(matrix[row_end][i])
+                    result += matrix[row_end][i],
                 row_end -= 1
 
+            # Example: [[1, 2], [3, 4]]
             if col_begin <= col_end:
                 for i in range(row_end, row_begin-1, -1):
-                    res.append(matrix[i][col_begin])
+                    result += matrix[i][col_begin],
                 col_begin += 1
-        return res
+
+        return result

aa9f466dcc0e877cc6daee99b8998af751f81872
Mingyu Liu <mliu@quantcast.com>
2016-11-11 11:07:53 -0800

    Update Edit Distance

--- leetcode/edit_distance.py
+++ leetcode/edit_distance.py
@@ -5,15 +5,20 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
-        dp = [[0 for j in xrange(len(word2)+1)] for i in xrange(len(word1)+1)]
-        for i in xrange(len(word1)+1):
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n+1) for _ in range(m+1)]
+
+        for i in range(1, n+1):
+            dp[0][i] = i
+        
+        for i in range(1, m+1):
             dp[i][0] = i
-        for j in xrange(len(word2)+1):
-            dp[0][j] = j
-        for i in xrange(1, len(word1)+1):
-            for j in xrange(1, len(word2)+1):
-                if word1[i-1] == word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
+        
+        for i in range(m):
+            for j in range(n):
+                if word1[i] == word2[j]:
+                    dp[i+1][j+1] = dp[i][j]
                 else:
-                    dp[i][j] = min(dp[i-1][j-1] + 1, dp[i][j-1] + 1, dp[i-1][j] + 1)
-        return dp[len(word1)][len(word2)]
+                    dp[i+1][j+1] = min(dp[i][j], dp[i+1][j], dp[i][j+1]) + 1
+        
+        return dp[m][n]

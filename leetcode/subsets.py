a6d7815b4c355eed6e5930796293771dfa103202
Mingyu Liu <mliu@quantcast.com>
2016-11-12 21:31:40 -0800

    Subsets

---
+++ leetcode/subsets.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.dfs(nums, 0, (), result)
+        return result
+
+    def dfs(self, nums, start, tmp, result):
+        result += tmp,
+        
+        for i in range(start, len(nums)):
+            self.dfs(nums, i+1, tmp+(nums[i],), result)

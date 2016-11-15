ce7ffd83808dabf1963b03de4a427091a54697fd
Mingyu Liu <mliu@quantcast.com>
2016-11-15 15:14:59 -0800

    Update Maximal Rectangle

--- leetcode/maximal_rectangle.py
+++ leetcode/maximal_rectangle.py
@@ -9,33 +9,36 @@ class Solution(object):
         if not matrix:
             return 0
 
-        row_length, column_length = len(matrix), len(matrix[0])
-        left, right, height = [0] * column_length, [column_length] * column_length, [0] * column_length
-        maximal = 0
-        for i in range(row_length):
-            cur_left, cur_right = 0, column_length
+        m, n = len(matrix), len(matrix[0])
+        height = [0] * n
+        left = [0] * n
+        right = [n] * n
+        _max = 0
 
-            for j in range(column_length):
+        for i in range(m):
+            for j in range(n):
                 if matrix[i][j] == '1':
                     height[j] += 1
                 else:
                     height[j] = 0
 
-            for j in range(column_length):
+            curr_left = 0
+            for j in range(n):
                 if matrix[i][j] == '1':
-                    left[j] = max(left[j], cur_left)
+                    left[j] = max(left[j], curr_left)
                 else:
-                    cur_left = j + 1
+                    curr_left = j + 1
                     left[j] = 0
 
-            for j in range(column_length-1, -1, -1):
+            curr_right = n
+            for j in range(n-1, -1, -1):
                 if matrix[i][j] == '1':
-                    right[j] = min(right[j], cur_right)
+                    right[j] = min(right[j], curr_right)
                 else:
-                    cur_right = j
-                    right[j] = column_length
+                    curr_right = j
+                    right[j] = n
 
-            for j in range(column_length):
-                maximal = max(maximal, (right[j] - left[j]) * height[j])
+            for j in range(n):
+                _max = max(_max, height[j] * (right[j] - left[j]))
 
-        return maximal
+        return _max

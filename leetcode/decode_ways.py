9305650dc42a9440742a466a6aca558c3ae991a4
Mingyu Liu <mliu@quantcast.com>
2016-11-16 10:42:10 -0800

    Decode Ways

--- leetcode/decode_ways.py
+++ leetcode/decode_ways.py
@@ -8,11 +8,13 @@ class Solution(object):
             return 0
 
         n = len(s)
-        res = [0] * (n+1)
-        res[n] = 1
-        res[n-1] = 1 if s[n-1] != '0' else 0
+        dp = [0] * (n+1)
+        dp[n] = 1
+        dp[n-1] = 1 if s[n-1] != '0' else 0
+
         for i in range(n-2, -1, -1):
             if s[i] == '0':
                 continue
-            res[i] = res[i+1]+res[i+2] if int(s[i:i+2]) <= 26 else res[i+1]
-        return res[0]
+            dp[i] = dp[i+1] + dp[i+2] if int(s[i:i+2]) <= 26 else dp[i+1]
+
+        return dp[0]

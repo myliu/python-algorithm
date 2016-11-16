166502665a8fdc207d17ba159061aea27885d744
Mingyu Liu <mliu@quantcast.com>
2016-11-15 20:44:32 -0800

    Update Maximal Square

--- leetcode/maximal_square.py
+++ leetcode/maximal_square.py
@@ -10,11 +10,11 @@ class Solution(object):
             return 0
 
         m, n = len(matrix), len(matrix[0])
-        dp = [[0 for j in range(n+1)] for i in range(m+1)]
-        result = 0
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                if matrix[i-1][j-1] == '1':
-                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
-                    result = max(result, dp[i][j])
-        return result*result
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        _max = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    dp[i+1][j+1] = min(dp[i][j], dp[i+1][j], dp[i][j+1]) + 1
+                    _max = max(_max, dp[i+1][j+1])
+        return _max**2

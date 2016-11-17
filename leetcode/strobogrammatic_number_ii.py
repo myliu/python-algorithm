e2eb3d5752329058dda0ea5a8e591b83af4c88a5
Mingyu Liu <mliu@quantcast.com>
2016-11-17 15:30:05 -0800

    Update Strobogrammatic Number II

--- leetcode/strobogrammatic_number_ii.py
+++ leetcode/strobogrammatic_number_ii.py
@@ -4,22 +4,26 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        def helper(m, n):
+        # Pass n as number cannot start with 0.
+        # We need to detect the top level of the recursion.
+        def dfs(m, n):
             if m == 0:
                 return ['']
 
             if m == 1:
                 return ['0', '1', '8']
 
-            prev = helper(m-2, n)
-            cur = []
+            prev = dfs(m-2, n)
+            curr = []
+
             for p in prev:
                 if m != n:
-                    cur.append('0' + p + '0')
-                cur.append('1' + p + '1')
-                cur.append('8' + p + '8')
-                cur.append('6' + p + '9')
-                cur.append('9' + p + '6')
-            return cur
+                    curr += '0' + p + '0',
+                curr += '1' + p + '1',
+                curr += '6' + p + '9',
+                curr += '8' + p + '8',
+                curr += '9' + p + '6',
 
-        return helper(n, n)
+            return curr
+
+        return dfs(n, n)

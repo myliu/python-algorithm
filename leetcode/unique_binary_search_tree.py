b735475de5de221ea132923cffc5fff65e20c3f9
Mingyu Liu <mliu@quantcast.com>
2016-11-16 21:00:32 -0800

    Update Unique Binary Search Tree

--- leetcode/unique_binary_search_tree.py
+++ leetcode/unique_binary_search_tree.py
@@ -1,12 +1,16 @@
 class Solution(object):
+
+    # https://discuss.leetcode.com/topic/8398/dp-solution-in-6-lines-with-explanation-f-i-n-g-i-1-g-n-i
     def numTrees(self, n):
         """
         :type n: int
         :rtype: int
         """
-        cache = [0] * (n+1)
-        cache[0] = cache[1] = 1
-        for i in xrange(2, n+1):
-            for j in xrange(1, i+1):
-                cache[i] += cache[j-1] * cache[i-j]
-        return cache[n]
+        dp = [0] * (n+1)
+        dp[0] = dp[1] = 1
+
+        for i in range(2, n+1):
+            for j in range(1, i+1):
+                dp[i] += dp[j-1] * dp[i-j]
+
+        return dp[n]

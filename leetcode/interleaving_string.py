2822b3a5bddeed7dec5de2499210e72e6ec1e3dc
Mingyu Liu <mliu@quantcast.com>
2016-11-16 22:54:51 -0800

    Update Interleaving String

--- leetcode/interleaving_string.py
+++ leetcode/interleaving_string.py
@@ -6,18 +6,37 @@ class Solution(object):
         :type s3: str
         :rtype: bool
         """
-        if len(s1) + len(s2) != len(s3):
+        m, n, k = len(s1), len(s2), len(s3)
+
+        if m + n != k:
             return False
 
-        dp = [[False for j in range(len(s2)+1)] for i in range(len(s1)+1)]
-        for i in range(len(s1)+1):
-            for j in range(len(s2)+1):
-                if i == 0 and j == 0:
-                    dp[i][j] = True
-                elif i == 0:
-                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[i+j-1]
-                elif j == 0:
-                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i+j-1]
-                else:
-                    dp[i][j] = (dp[i][j-1] and s2[j-1] == s3[i+j-1]) or (dp[i-1][j] and s1[i-1] == s3[i+j-1])
-        return dp[len(s1)][len(s2)]
+        dp = [[False for _ in range(n+1)] for _ in range(m+1)]
+        dp[0][0] = True
+
+        for i in range(m):
+            dp[i+1][0] = dp[i][0] and s1[i] == s3[i]
+
+        for j in range(n):
+            dp[0][j+1] = dp[0][j] and s2[j] == s3[j]
+
+        for i in range(m):
+            for j in range(n):
+                dp[i+1][j+1] = (dp[i][j+1] and s1[i] == s3[i+j+1]) or (dp[i+1][j] and s2[j] == s3[i+j+1])
+
+        return dp[m][n]
+
+"""
+s1 = "aabcc"
+s2 = "dbbca"
+s3 = "aadbbcbcac"
+
+[
+    [True, False, False, False, False, False],
+    [True, False, False, False, False, False],
+    [True, True, True, True, True, False],
+    [False, True, True, False, True, False],
+    [False, False, True, True, True, True],
+    [False, False, False, True, False, True]
+]
+"""

57d499fdbab4e7dc6bf6ed329e7a0a1f34957b86
Mingyu Liu <mliu@quantcast.com>
2016-11-19 14:17:45 -0800

    Update Construct Binary Tree from Preorder and Inorder Traversal

--- leetcode/construct_binary_tree_from_preorder_and_inorder_traversal_v1.py
+++ leetcode/construct_binary_tree_from_preorder_and_inorder_traversal_v1.py
@@ -15,10 +15,12 @@ class Solution(object):
         def helper(pre_start, in_start, in_end, preorder, inorder):
             if pre_start >= len(preorder) or in_start > in_end:
                 return None
-            root = TreeNode(preorder[pre_start])
-            in_mid = inorder.index(preorder[pre_start])
-            root.left = helper(pre_start+1, in_start, in_mid-1, preorder, inorder)
-            root.right = helper(pre_start+1+in_mid-in_start, in_mid+1, in_end, preorder, inorder)
+
+            val = preorder[pre_start]
+            i = inorder.index(val)
+            root = TreeNode(val)
+            root.left = helper(pre_start+1, in_start, i-1, preorder, inorder)
+            root.right = helper(pre_start+1+i-in_start, i+1, in_end, preorder, inorder)
             return root
 
         return helper(0, 0, len(inorder)-1, preorder, inorder)

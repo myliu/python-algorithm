c3faefcbe4cd4b27c34c635f48c2f8b9ea8717c3
Mingyu Liu <mliu@quantcast.com>
2016-11-20 08:18:41 -0800

    Update Path Sum III

--- leetcode/path_sum_iii.py
+++ leetcode/path_sum_iii.py
@@ -6,13 +6,19 @@
 #         self.right = None
 
 class Solution(object):
+
+    """
+    The difference between pathSum and path_sum_from_root is that
+    path_sum_from_root returns the count of paths, where each path starts from root,
+    but may or may not end at leaf.
+    """
     def pathSum(self, root, sum):
         """
         :type root: TreeNode
         :type sum: int
         :rtype: int
         """
-        def dfs(root, sum):
+        def path_sum_from_root(root, sum):
             if not root:
                 return 0
 
@@ -20,10 +26,10 @@ class Solution(object):
             if sum == root.val:
                 count += 1
 
-            count += dfs(root.left, sum-root.val)
-            count += dfs(root.right, sum-root.val)
+            count += path_sum_from_root(root.left, sum-root.val)
+            count += path_sum_from_root(root.right, sum-root.val)
             return count
 
         if not root:
             return 0
-        return dfs(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
+        return path_sum_from_root(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)

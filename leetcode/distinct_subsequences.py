b3a12da97cb8a8bebf744cfb743453bba52b93b8
Mingyu Liu <mliu@quantcast.com>
2016-11-20 22:56:37 -0800

    Update Distinct Subsequences

--- leetcode/distinct_subsequences.py
+++ leetcode/distinct_subsequences.py
@@ -20,16 +20,16 @@ class Solution(object):
         :type t: str
         :rtype: int
         """
-        dp = [[0 for j in range(len(t)+1)] for i in range(len(s)+1)]
+        dp = [[0 for _ in range(len(t)+1)] for _ in range(len(s)+1)]
 
         for i in range(len(s)+1):
             dp[i][0] = 1
 
-        for i in range(1, len(s)+1):
-            for j in range(1, len(t)+1):
-                if s[i-1] == t[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+        for i in range(len(s)):
+            for j in range(len(t)):
+                if s[i] == t[j]:
+                    dp[i+1][j+1] = dp[i][j] + dp[i][j+1]
                 else:
-                    dp[i][j] = dp[i-1][j]
+                    dp[i+1][j+1] = dp[i][j+1]
 
         return dp[len(s)][len(t)]

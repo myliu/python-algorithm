5c9b75e138dba268e6f374328ce56df8f509723d
Mingyu Liu <mliu@quantcast.com>
2016-11-26 15:23:07 -0800

    Update Clone Graph

--- leetcode/clone_graph.py
+++ leetcode/clone_graph.py
@@ -1,26 +1,24 @@
 # Definition for a undirected graph node
-# class UndirectedGraphNode(object):
+# class UndirectedGraphNode:
 #     def __init__(self, x):
 #         self.label = x
 #         self.neighbors = []
 
-class Solution(object):
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
     def cloneGraph(self, node):
-        """
-        :type node: UndirectedGraphNode
-        :rtype: UndirectedGraphNode
-        """
-        def dfs(node, graph_dict):
+        def dfs(node, graph):
             if not node:
                 return None
 
-            if node.label in graph_dict:
-                return graph_dict[node.label]
+            if node.label in graph:
+                return graph[node.label]
 
             clone = UndirectedGraphNode(node.label)
-            graph_dict[node.label] = clone
+            graph[clone.label] = clone
             for node in node.neighbors:
-                clone.neighbors.append(dfs(node, graph_dict))
+                clone.neighbors += dfs(node, graph),
             return clone
 
         return dfs(node, {})

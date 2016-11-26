8a53136f14366e5b9f08cefc61241e6a1a19c1a9
Mingyu Liu <mliu@quantcast.com>
2016-11-26 09:19:32 -0800

    Update Surrounded Regions

--- leetcode/surrounded_regions.py
+++ leetcode/surrounded_regions.py
@@ -4,41 +4,38 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
-        # 1- Turn 'O' on the border to '1'
-        # 2- Turn remaining 'O' to 'X'
-        # 3- Turn '1' to 'O'
-        def check(board, i, j, row, col):
+        def check(i, j):
             if board[i][j] == 'O':
                 board[i][j] = '1'
                 if i - 1 > 0:
-                    check(board, i-1, j, row, col)
+                    check(i-1, j)
+                if i + 1 < m - 1:
+                    check(i+1, j)
                 if j - 1 > 0:
-                    check(board, i, j-1, row, col)
-                if i + 1 < row - 1:
-                    check(board, i+1, j, row, col)
-                if j + 1 < col - 1:
-                    check(board, i, j+1, row, col)
+                    check(i, j-1)
+                if j + 1 < n - 1:
+                    check(i, j+1)
 
         if not board:
             return
 
-        row, col = len(board), len(board[0])
-        for i in range(row):
-            check(board, i, 0, row, col)
-            if col > 1:
-                check(board, i, col-1, row, col)
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            check(i, 0)
+            if n > 1:
+                check(i, n-1)
 
-        for j in range(1, col-1):
-            check(board, 0, j, row, col)
-            if row > 1:
-                check(board, row-1, j, row, col)
+        for j in range(n):
+            check(0, j)
+            if m > 1:
+                check(m-1, j)
 
-        for i in range(row):
-            for j in range(col):
+        for i in range(m):
+            for j in range(n):
                 if board[i][j] == 'O':
                     board[i][j] = 'X'
 
-        for i in range(row):
-            for j in range(col):
+        for i in range(m):
+            for j in range(n):
                 if board[i][j] == '1':
                     board[i][j] = 'O'

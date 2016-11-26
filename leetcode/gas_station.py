cc32a26a397f7f3652214a9c3900b5b2a3c06e1b
Mingyu Liu <mliu@quantcast.com>
2016-11-26 15:45:31 -0800

    Update Gas Station

--- leetcode/gas_station.py
+++ leetcode/gas_station.py
@@ -1,4 +1,6 @@
 class Solution(object):
+
+    # https://discuss.leetcode.com/topic/1344/share-some-of-my-ideas
     def canCompleteCircuit(self, gas, cost):
         """
         :type gas: List[int]
@@ -8,9 +10,9 @@ class Solution(object):
         if not gas or not cost or sum(gas) < sum(cost):
             return -1
 
-        pos, balance = 0, 0
+        start, balance = 0, 0
         for i in range(len(gas)):
             balance += gas[i] - cost[i]
             if balance < 0:
-                pos, balance = i+1, 0
-        return pos
+                start, balance = i+1, 0
+        return start

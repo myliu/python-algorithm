27f44be23f6738d9af4979fcc6f62da38fe0a170
Mingyu Liu <mliu@quantcast.com>
2016-11-26 10:27:51 -0800

    Update Palindrome Partitioning

--- leetcode/palindrome_partitioning.py
+++ leetcode/palindrome_partitioning.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: List[List[str]]
         """
-        results = []
+        result = []
         for i in range(1, len(s)+1):
             if s[:i] == s[i-1::-1]:
-                for rest in self.partition(s[i:]):
-                    results.append([s[:i]] + rest)
-
-        if not results:
-            return[[]]
-
-        return results
+                for p in self.partition(s[i:]):
+                    result += [s[:i]] + p,
+        
+        if not result:
+            return [[]]
+        
+        return result

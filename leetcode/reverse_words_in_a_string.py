7ba97532cd5238b9f1b289e6c599506ddee5bbf0
Mingyu Liu <mliu@quantcast.com>
2016-11-27 20:44:24 -0800

    Update Reverse Words in a String

--- leetcode/reverse_words_in_a_string.py
+++ leetcode/reverse_words_in_a_string.py
@@ -4,4 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        return ' '.join(s.strip().split()[::-1])
+        """
+        str.split([sep[, maxsplit]])
+        If sep is not specified or is None, a different splitting algorithm is applied:
+        runs of consecutive whitespace are regarded as a single separator,
+        and the result will contain no empty strings at the start or end if the string has leading or trailing whitespace.
+        Consequently, splitting an empty string or a string consisting of just whitespace with a None separator returns [].
+        """
+        return ' '.join(s.split()[::-1])

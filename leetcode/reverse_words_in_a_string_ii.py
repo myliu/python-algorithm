ae1832c353b373a67e0a675a698a31e4f3ca498a
Mingyu Liu <mliu@quantcast.com>
2016-11-27 21:35:17 -0800

    Update Reverse Words in a String II

--- leetcode/reverse_words_in_a_string_ii.py
+++ leetcode/reverse_words_in_a_string_ii.py
@@ -1,17 +1,23 @@
-class Solution(object):
+class Solution:
+    # @param s, a list of 1 length strings, e.g., s = ['h','e','l','l','o']
+    # @return nothing
+    # Three step to reverse.
     def reverseWords(self, s):
-        """
-        :type s: a list of 1 length strings (List[str])
-        :rtype: nothing
-        """
-        self.reverse(s, 0, (len(s)-1))
+        n = len(s)
+
+        # 1. Reverse the whole sentence.
+        self.reverse(s, 0, n-1)
+
+        # 2. Reverse each word.
         start = i = 0
-        while i < len(s):
+        while i < n:
             if s[i] == ' ':
                 self.reverse(s, start, i-1)
                 start = i + 1
             i += 1
-        self.reverse(s, start, (len(s)-1))
+
+        # 3. Reverse the last word, if there is only one word this will solve the corner case.
+        self.reverse(s, start, n-1)
 
     def reverse(self, s, i, j):
         while i < j:

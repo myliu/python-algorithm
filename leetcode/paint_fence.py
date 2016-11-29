173070d9180714e94a6d41350ad8496b43f5acfa
Mingyu Liu <mliu@quantcast.com>
2016-11-28 22:56:54 -0800

    Update Paint Fence

--- leetcode/paint_fence.py
+++ leetcode/paint_fence.py
@@ -1,4 +1,6 @@
 class Solution(object):
+
+    # https://discuss.leetcode.com/topic/23426/o-n-time-java-solution-o-1-space
     def numWays(self, n, k):
         """
         :type n: int
@@ -12,10 +14,7 @@ class Solution(object):
         # n == 2
         diff = k * (k-1)
         same = k
-        i = 2
-        while i < n:
-            tmp = diff
-            diff = same * (k-1) + diff * (k-1)
-            same = tmp
-            i += 1
+        # n >= 3
+        for i in range(2, n):
+            diff, same = (same + diff) * (k - 1), diff
         return diff + same

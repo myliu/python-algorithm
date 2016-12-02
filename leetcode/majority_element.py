7e5632d8d38a30e00c2c30e0fd9ea37f574c713b
Mingyu Liu <mliu@quantcast.com>
2016-12-02 15:25:29 -0800

    Update Majority Element

--- leetcode/majority_element.py
+++ leetcode/majority_element.py
@@ -1,22 +1,20 @@
-class Solution:
-    # @param num, a list of integers
-    # @return an integer
-    def majorityElement(self, num):
-        # 115 ms solution -- O(nlogn)
-        # return sorted(num)[len(num)/2]
-
-        # 126 ms solution -- O(n)
-        m = num[0]
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        majority = nums[0]
         count = 1
-        for i in range(1, len(num)):
-            if count == 0:
-                m = num[i]
+        for num in nums[1:]:
+            if not count:
+                majority = num
                 count = 1
-            elif num[i] == m:
+            elif num == majority:
                 count += 1
             else:
                 count -= 1
-        return m
+        return majority
 
 if __name__ == '__main__':
     s = Solution()

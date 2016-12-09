77ee630871d06037784b2a83402100b6529bdcf6
Mingyu Liu <mliu@quantcast.com>
2016-12-09 14:45:41 -0800

    Update Majority Element II

--- leetcode/majority_element_ii.py
+++ leetcode/majority_element_ii.py
@@ -11,11 +11,11 @@ class Solution(object):
             elif num == candidate2:
                 count2 += 1
             else:
-                if count1 == 0:
+                if not count1:
                     candidate1, count1 = num, 1
-                elif count2 == 0:
+                elif not count2:
                     candidate2, count2 = num, 1
                 else:
                     count1 -= 1
                     count2 -= 1
-        return [n for n in (candidate1, candidate2) if nums.count(n) > len(nums)/3]
+        return [c for c in (candidate1, candidate2) if nums.count(c) > len(nums)/3]

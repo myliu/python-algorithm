40d9ddad38093659897b4292097950bc11e030c7
Mingyu Liu <mliu@quantcast.com>
2016-12-09 16:55:56 -0800

    Update Kth Smallest Element in a BST

--- leetcode/kth_smallest_element_in_bst.py
+++ leetcode/kth_smallest_element_in_bst.py
@@ -1,44 +1,32 @@
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution(object):
+    
     count = 0
-
+    
     def kthSmallest(self, root, k):
-        if root == None:
-            return
-
-        res = self.kthSmallest(root.left, k)
-        if res != None:
-            return res
-
-        self.count = self.count + 1
-        print self.count
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        if not root:
+            return None
+        
+        result = self.kthSmallest(root.left, k)
+        # This line cannot be replaced with `if result` because it is possible result == 0
+        if result != None:
+            return result
+        
+        self.count += 1
         if self.count == k:
             return root.val
-
-        res = self.kthSmallest(root.right, k)
-        if res != None:
-            return res
-
-
-if __name__ == '__main__':
-    t1 = TreeNode(10)
-    t2 = TreeNode(5)
-    t3 = TreeNode(15)
-    t4 = TreeNode(12)
-    t5 = TreeNode(18)
-
-    t1.left = t2
-    t1.right = t3
-    t3.left = t4
-    t3.right = t5
-
-    s = Solution()
-
-    print s.kthSmallest(t1, 7)
+        
+        result = self.kthSmallest(root.right, k)
+        if result != None:
+            return result

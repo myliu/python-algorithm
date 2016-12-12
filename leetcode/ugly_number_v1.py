c770c4a4e64f0b93f449e8b5580ba849ccbb8604
Mingyu Liu <mliu@quantcast.com>
2016-12-11 22:03:26 -0800

    Update Ugly Number - Version 1

--- leetcode/ugly_number_v1.py
+++ leetcode/ugly_number_v1.py
@@ -7,7 +7,7 @@ class Solution(object):
         if num == 0:
             return False
 
-        for prime in [2, 3, 5]:
+        for prime in (2, 3, 5):
             while num % prime == 0:
                 num /= prime
         return num == 1

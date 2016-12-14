ae189624273cafc575f1f1dbd0205c1b7d87cbb0
Mingyu Liu <mliu@quantcast.com>
2016-12-13 21:30:10 -0800

    Update Alien Dictionary

--- leetcode/alien_dictionary.py
+++ leetcode/alien_dictionary.py
@@ -10,29 +10,33 @@ class Solution(object):
         degrees = dict.fromkeys(set(''.join(words)), 0)
 
         for i in range(len(words)-1):
-            current = words[i]
-            next = words[i+1]
-            length = min(len(current), len(next))
+            curr = words[i]
+            _next = words[i+1]
+            length = min(len(curr), len(_next))
             for j in range(length):
-                if current[j] == next[j]:
+                if curr[j] == _next[j]:
                     continue
-                if next[j] not in orders[current[j]]:
-                    orders[current[j]].add(next[j])
-                    degrees[next[j]] += 1
-                    break
+                if _next[j] not in orders[curr[j]]:
+                    orders[curr[j]].add(_next[j])
+                    degrees[_next[j]] += 1
+                # The loop need to be broken anyway if curr[j] != _next[j]
+                break
+            else:
+                if curr[:length] == _next[:length] and len(curr) > len(_next):
+                    return ''
 
         queue = deque()
         for k, v in degrees.items():
             if not v:
-                queue.append(k)
+                queue += k,
 
         result = ''
         while queue:
-            char = queue.popleft()
-            result += char
-            tos = orders[char]
-            for to in tos:
-                degrees[to] -= 1
-                if not degrees[to]:
-                    queue.append(to)
+            curr = queue.popleft()
+            result += curr
+            _next = orders[curr]
+            for c in _next:
+                degrees[c] -= 1
+                if not degrees[c]:
+                    queue += c,
         return result if len(result) == len(degrees) else ''

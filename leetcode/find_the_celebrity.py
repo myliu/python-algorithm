149f950b8dfb8b1702eb2f6169f5fb4dee52659f
Mingyu Liu <mliu@quantcast.com>
2016-12-14 16:52:23 -0800

    Update Find the Celebrity

--- leetcode/find_the_celebrity.py
+++ leetcode/find_the_celebrity.py
@@ -11,12 +11,15 @@ class Solution(object):
         :rtype: int
         """
         candidate = 0
+
+        # Find celebrity
         for i in range(1, n):
             if knows(candidate, i):
                 candidate = i
 
-        for i in range(0, n):
-            if i != candidate and (knows(candidate, i) or not knows(i, candidate)):
+        # Verify celebrity
+        for i in range(n):
+            if i != candidate and (not knows(i, candidate) or knows(candidate, i)):
                 return -1
 
         return candidate

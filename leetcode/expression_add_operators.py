7f319df759b3c71c60f3ceded049a06f07ddf2bf
Mingyu Liu <mliu@quantcast.com>
2016-12-16 14:47:24 -0800

    Update Expression and Operators

--- leetcode/expression_add_operators.py
+++ leetcode/expression_add_operators.py
@@ -5,24 +5,24 @@ class Solution(object):
         :type target: int
         :rtype: List[str]
         """
-        def helper(num, target, pos, path, val, multiplier, res):
-            if pos == len(num):
-                if val == target:
-                    res.append(path)
-
-            for i in xrange(pos, len(num)):
-                if i > pos and num[pos] == '0':
-                    break
-
-                current = int(num[pos:i+1])
-                if pos == 0:
-                    helper(num, target, i+1, str(current), current, current, res)
-                else:
-                    helper(num, target, i+1, path + '+' + str(current), val + current, current, res)
-                    helper(num, target, i+1, path + '-' + str(current), val - current, -current, res)
-                    helper(num, target, i+1, path + '*' + str(current), val - multiplier + multiplier * current, multiplier * current, res)
-
-        res = []
+        result = []
         # The initial path, val and multiplier do not matter, and they will be overriden later
-        helper(num, target, 0, 'xxx', 888, 999, res)
-        return res
+        self.dfs(num, target, 0, '', 0, 0, result)
+        return result
+
+    def dfs(self, num, target, pos, path, val, multiplier, result):
+        if pos == len(num):
+            if val == target:
+                result += path,
+
+        for i in xrange(pos, len(num)):
+            if i > pos and num[pos] == '0':
+                break
+
+            curr = int(num[pos:i+1])
+            if pos == 0:
+                self.dfs(num, target, i+1, str(curr), curr, curr, result)
+            else:
+                self.dfs(num, target, i+1, path+'+'+str(curr), val+curr, curr, result)
+                self.dfs(num, target, i+1, path+'-'+str(curr), val-curr, -curr, result)
+                self.dfs(num, target, i+1, path+'*'+str(curr), val-multiplier+multiplier*curr, multiplier*curr, result)

7f5079c7ec26f2012a08896a8b63081a11aaa8a7
Mingyu Liu <mliu@quantcast.com>
2016-12-18 09:35:59 -0800

    Update Find the Duplicate Number

--- leetcode/find_the_duplicate_number.py
+++ leetcode/find_the_duplicate_number.py
@@ -6,12 +6,21 @@ class Solution(object):
         """
         slow = nums[0]
         fast = nums[nums[0]]
+
+        # To find the meeting point.
+        # https://discuss.leetcode.com/topic/19367/java-o-1-space-solution-with-detailed-explanation/24
+        # Distance to meeting point for fast pointer a + b + (b + c)
+        # Distance to meeting point for slow pointer a + b
+        # a + b + (b + c) = 2(a + b)
+        # c = a
         while slow != fast:
             slow = nums[slow]
             fast = nums[nums[fast]]
 
+        # To find the entry point, which is the duplicate number.
         slow = 0
         while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
+
         return slow

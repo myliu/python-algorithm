698e64fa5dd0e97272c4e5f4e51c94be800588ad
Mingyu Liu <mliu@quantcast.com>
2016-12-18 21:32:53 -0800

    Update Word Pattern II

--- leetcode/word_pattern_ii.py
+++ leetcode/word_pattern_ii.py
@@ -5,37 +5,35 @@ class Solution(object):
         :type str: str
         :rtype: bool
         """
-        def is_match(pattern, i, str, j, d, s):
-            if i >= len(pattern) and j >= len(str):
-                return True
-            elif i >= len(pattern) and j < len(str):
-                return False
-            elif i < len(pattern) and j >= len(str):
-                return False
+        d, s = dict(), set()
+        return self.is_match(pattern, 0, str, 0, d, s)
 
-            next_pattern = pattern[i]
-            if next_pattern in d:
-                next_word = d[next_pattern]
-                if not str.startswith(next_word, j):
-                    return False
-                return is_match(pattern, i+1, str, j+len(next_word), d, s)
-
-            for k in range(len(str)-j):
-                next_word = str[j:j+k+1]
-
-                if next_word in s:
-                    continue
-
-                d[next_pattern] = next_word
-                s.add(next_word)
-
-                if is_match(pattern, i+1, str, j+len(next_word), d, s):
-                    return True
-
-                del d[next_pattern]
-                s.remove(next_word)
+    def is_match(self, pattern, i, string, j, d, s):
+        if i >= len(pattern) and j >= len(string):
+            return True
+        elif i >= len(pattern) and j < len(string):
+            return False
+        elif i < len(pattern) and j >= len(string):
             return False
 
-        d = {}
-        s = set()
-        return is_match(pattern, 0, str, 0, d, s)
+        next_pattern = pattern[i]
+        if next_pattern in d:
+            next_word = d[next_pattern]
+            if not string[j:].startswith(next_word):
+                return False
+            return self.is_match(pattern, i+1, string, j+len(next_word), d, s)
+
+        for k in range(len(string)-j):
+            next_word = string[j:j+k+1]
+
+            if next_word in s:
+                continue
+
+            s.add(next_word)
+            d[next_pattern] = next_word
+            if self.is_match(pattern, i+1, string, j+k+1, d, s):
+                return True
+            s.remove(next_word)
+            del d[next_pattern]
+
+        return False

3d71d4e39ae33d7f894b2348f8e288081d5c3f41
Mingyu Liu <mliu@quantcast.com>
2016-12-18 23:21:03 -0800

    Update Best Meeting Point

--- leetcode/best_meeting_point.py
+++ leetcode/best_meeting_point.py
@@ -4,20 +4,19 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        def find_min(person):
-            length = len(person)
-            i, j, result = 0, length - 1, 0
-            while i < j:
-                result += person[j] - person[i]
-                i += 1
-                j -= 1
-            return result
-
-        row, column = [], []
-        for i in xrange(len(grid)):
-            for j in xrange(len(grid[0])):
+        m, n = len(grid), len(grid[0])
+        row, col = [], []
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == 1:
-                    row.append(i)
-                    column.append(j)
+                    row += i,
+                    col += j,
+        return self.min_dist(sorted(row)) + self.min_dist(sorted(col))
 
-        return find_min(sorted(row)) + find_min(sorted(column))
+    def min_dist(self, people):
+        i, j, dist = 0, len(people)-1, 0
+        while i < j:
+            dist += people[j] - people[i]
+            i += 1
+            j -= 1
+        return dist

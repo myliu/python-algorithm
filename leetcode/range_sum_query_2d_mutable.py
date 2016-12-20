307c9119d0483cb319f129717dceec3c9b5fb8d3
Mingyu Liu <mliu@quantcast.com>
2016-12-20 10:25:28 -0800

    Update Range Sum Query 2D - Mutable

--- leetcode/range_sum_query_2d_mutable.py
+++ leetcode/range_sum_query_2d_mutable.py
@@ -6,24 +6,12 @@ class NumMatrix(object):
         """
         if not matrix or not matrix[0]:
             return
-
         m, n = len(matrix), len(matrix[0])
         self.matrix = matrix
         self.bit = [[0 for _ in range(n+1)] for _ in range(m+1)]
-        for i in range(m):
-            for j in range(n):
-                self.init(i, j, matrix[i][j])
-
-    def init(self, row, col, val):
-        matrix = self.matrix
-        m, n = len(matrix), len(matrix[0])
-        i = row + 1
-        while i <= m:
-            j = col + 1
-            while j <= n:
-                self.bit[i][j] += val
-                j += j & (-j)
-            i += i & (-i)
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                self._update(i, j, matrix[i-1][j-1])
 
     def update(self, row, col, val):
         """
@@ -33,11 +21,19 @@ class NumMatrix(object):
         :type val: int
         :rtype: void
         """
-        matrix = self.matrix
-        m, n = len(matrix), len(matrix[0])
-        delta = val - matrix[row][col]
-        matrix[row][col] = val
-        self.init(row, col, delta)
+        delta = val - self.matrix[row][col]
+        self._update(row+1, col+1, delta)
+        self.matrix[row][col] = val
+        
+    def _update(self, row, col, val):
+        m, n = len(self.matrix), len(self.matrix[0])
+        i = row
+        while i <= m:
+            j = col
+            while j <= n:
+                self.bit[i][j] += val
+                j += j & (-j)
+            i += i & (-i)
 
     def sumRegion(self, row1, col1, row2, col2):
         """
@@ -48,12 +44,11 @@ class NumMatrix(object):
         :type col2: int
         :rtype: int
         """
-        get_sum = self.get_sum
-        return get_sum(row2+1, col2+1) - get_sum(row2+1, col1) - get_sum(row1, col2+1) + get_sum(row1, col1)
-
-    def get_sum(self, row, col):
+        return self._sum(row2+1, col2+1) - self._sum(row1, col2+1) - self._sum(row2+1, col1) + self._sum(row1, col1)
+        
+    def _sum(self, row, col):
+        i = row
         result = 0
-        i, j = row, col
         while i > 0:
             j = col
             while j > 0:
@@ -61,6 +56,12 @@ class NumMatrix(object):
                 j -= j & (-j)
             i -= i & (-i)
         return result
+
+# Your NumMatrix object will be instantiated and called as such:
+# numMatrix = NumMatrix(matrix)
+# numMatrix.sumRegion(0, 1, 2, 3)
+# numMatrix.update(1, 1, 10)
+# numMatrix.sumRegion(1, 2, 3, 4)
 
 if __name__ == '__main__':
     matrix = [
@@ -71,6 +72,7 @@ if __name__ == '__main__':
       [1, 0, 3, 0, 5]
     ]
     numMatrix = NumMatrix(matrix)
+    print numMatrix.bit
     print numMatrix.sumRegion(0, 1, 2, 3)
     numMatrix.update(1, 1, 10)
     print numMatrix.sumRegion(1, 2, 3, 4)

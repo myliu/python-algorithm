cffed542d59d95dcd8e9cdb96962b02e47c7b3c6
Mingyu Liu <mliu@quantcast.com>
2016-12-20 10:54:55 -0800

    Update Range Sum Query 2D - Immutable

--- leetcode/range_sum_query_2d_immutable.py
+++ leetcode/range_sum_query_2d_immutable.py
@@ -6,12 +6,12 @@ class NumMatrix(object):
         """
         if not matrix or not matrix[0]:
             return
-
         m, n = len(matrix), len(matrix[0])
-        dp = self.dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        self.matrix = matrix
+        self.dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
         for i in range(1, m+1):
             for j in range(1, n+1):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i-1][j-1]
+                self.dp[i][j] = self.dp[i-1][j] + self.dp[i][j-1] - self.dp[i-1][j-1] + matrix[i-1][j-1]
 
     def sumRegion(self, row1, col1, row2, col2):
         """
@@ -22,9 +22,7 @@ class NumMatrix(object):
         :type col2: int
         :rtype: int
         """
-        dp = self.dp
-        return dp[row2+1][col2+1] - dp[row1][col2+1] - dp[row2+1][col1] + dp[row1][col1]
-
+        return self.dp[row2+1][col2+1] - self.dp[row2+1][col1] - self.dp[row1][col2+1] + self.dp[row1][col1]
 
 # Your NumMatrix object will be instantiated and called as such:
 # numMatrix = NumMatrix(matrix)

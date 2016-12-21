172f3ee23b200c260afe4c216d9924e19298b213
Mingyu Liu <mliu@quantcast.com>
2016-12-21 14:32:29 -0800

    Update Best Time to Buy and Sell Stock with Cooldown

--- leetcode/best_time_to_buy_and_sell_stock_with_cooldown.py
+++ leetcode/best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -1,10 +1,12 @@
 class Solution(object):
+
+    # https://discuss.leetcode.com/topic/30431/easiest-java-solution-with-explanations
     def maxProfit(self, prices):
         """
         :type prices: List[int]
         :rtype: int
         """
-        if not prices or len(prices) <= 1:
+        if not prices or len(prices) == 1:
             return 0
 
         b1 = -prices[0]
@@ -13,5 +15,5 @@ class Solution(object):
             b0 = max(b1, s2-prices[i])
             s0 = max(s1, b1+prices[i])
             b1 = b0
-            s2, s1 = s1, s0
+            s1, s2 = s0, s1
         return s0

07e2221a7f579808cb26d73920d71508ab8c13be
Mingyu Liu <mliu@quantcast.com>
2016-12-20 23:26:16 -0800

    Update Number of Islands II

--- leetcode/number_of_islands_ii.py
+++ leetcode/number_of_islands_ii.py
@@ -7,15 +7,15 @@ class Solution(object):
         :rtype: List[int]
         """
         def valid(i, j, grid):
-            if 0 <= i < m and 0 <= j < n and grid[i][j] == 1:
+            if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == 1:
                 return True
             return False
 
         def find(node):
-            if nodes[node] != node:
+            if node != nodes[node]:
                 nodes[node] = find(nodes[node])
             return nodes[node]
-        
+
         def union(node1, node2):
             x, y = find(node1), find(node2)
             if x == y:
@@ -23,22 +23,20 @@ class Solution(object):
             nodes[x] = y
             return 1
 
-        grid = [[0 for j in range(n)] for i in range(m)]
+        grid = [[0 for _ in range(n)] for _ in range(m)]
         nodes = {}
         counts = []
         count = 0
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for pos in positions:
+        for position in positions:
             count += 1
-            i, j = pos[0], pos[1]
+            i, j = position[0], position[1]
             grid[i][j] = 1
             nodes[(i, j)] = (i, j)
-            for d in directions:
-                x, y = i+d[0], j+d[1]
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                x, y = i + dx, j + dy
                 if valid(x, y, grid):
                     count -= union((i, j), (x, y))
-            counts.append(count)
-
+            counts += count,
         return counts
 
 if __name__ == '__main__':

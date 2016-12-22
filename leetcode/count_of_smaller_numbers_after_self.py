f9f05f05bdfdf71f53ce30407c63feeb769884af
Mingyu Liu <mliu@quantcast.com>
2016-12-22 11:13:09 -0800

    Update Count of Smaller Numbers After Self

--- leetcode/count_of_smaller_numbers_after_self.py
+++ leetcode/count_of_smaller_numbers_after_self.py
@@ -4,25 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        bit = [0] * (n+1)
-        rank = {v: i+1 for i, v in enumerate(sorted(nums))}
-        res = []
-
-        def update(i, val):
-            while i < n:
+        # 1-based
+        def _update(i, val):
+            while i < len(bit):
                 bit[i] += val
-                i += i & -i
+                i += i & (-i)
 
-        def sum(i):
-            s = 0
+        # 1-based    
+        def _sum(i):
+            result = 0
             while i > 0:
-                s += bit[i]
-                i -= i & -i
-            return s
+                result += bit[i]
+                i -= i & (-i)
+            return result
 
-        for i in reversed(nums):
-            res.append(sum(rank[i]-1))
-            update(rank[i], 1)
+        n = len(nums)
+        # How many numbers are equal to or smaller than the index (1-based)
+        bit = [0] * (n+1)
+        ranks = {num: i+1 for i, num in enumerate(sorted(set(nums)))}
 
-        return res[::-1]
+        counts = []
+        for i in range(n-1, -1, -1):
+            num = nums[i]
+            rank = ranks[num]
+            counts += _sum(rank-1),
+            _update(rank, 1)
+        return counts[::-1]

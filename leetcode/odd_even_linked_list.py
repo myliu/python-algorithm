83c2076710332a7cff874b4619d19e99ca029c97
Mingyu Liu <mliu@quantcast.com>
2016-12-23 22:56:51 -0800

    Update Odd Even Linked List

--- leetcode/odd_even_linked_list.py
+++ leetcode/odd_even_linked_list.py
@@ -12,11 +12,10 @@ class Solution(object):
         """
         dummy1 = odd = ListNode(0)
         dummy2 = even = ListNode(0)
+
         while head:
-            odd.next = head
-            even.next = head.next
-            odd = odd.next
-            even = even.next
-            head = head.next.next if even else None
+            odd.next, even.next = head, head.next
+            odd, even = odd.next, even.next
+            head = head.next.next if head.next else None
         odd.next = dummy2.next
         return dummy1.next

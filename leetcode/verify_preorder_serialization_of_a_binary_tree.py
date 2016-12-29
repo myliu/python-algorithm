d0049ce05aff5240c6639b254725954d032f7d2b
Mingyu Liu <mliu@quantcast.com>
2016-12-29 15:34:13 -0800

    Update Verify Preorder Serialization of a Binary Tree

--- leetcode/verify_preorder_serialization_of_a_binary_tree.py
+++ leetcode/verify_preorder_serialization_of_a_binary_tree.py
@@ -1,16 +1,17 @@
 class Solution(object):
+
+    # https://discuss.leetcode.com/topic/35976/7-lines-easy-java-solution
     def isValidSerialization(self, preorder):
         """
         :type preorder: str
         :rtype: bool
         """
         tokens = preorder.split(',')
-        # Compensate for root, which has no in-degree
-        degree = -1
+        degree = 1
         for token in tokens:
-            degree += 1
-            if degree > 0:
+            degree -= 1
+            if degree < 0:
                 return False
             if token != '#':
-                degree -= 2
+                degree += 2
         return degree == 0

de88840678a7c462529bf2c27f8b1a9ffc562531
Mingyu Liu <mliu@quantcast.com>
2016-12-30 17:00:55 -0800

    Counting Bits - Version 2

---
+++ leetcode/counting_bits_v2.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        dp = [0] * (num+1)
+        for i in range(1, num+1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp

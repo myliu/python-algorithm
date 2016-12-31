70b3cf4c4ee9bd64a897ff094a66a745af43e34f
Mingyu Liu <mliu@quantcast.com>
2016-12-31 11:57:58 -0800

    Update Nested List Weight Sum

---
+++ leetcode/nested_list_weight_sum.py
@@ -0,0 +1,41 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution(object):
+    def depthSum(self, nestedList):
+        """
+        :type nestedList: List[NestedInteger]
+        :rtype: int
+        """
+        def helper(nestedList, depth):
+            _sum = 0
+            for nested_int in nestedList:
+                if nested_int.isInteger():
+                    _sum += nested_int.getInteger() * depth
+                else:
+                    _sum += helper(nested_int.getList(), depth+1)
+            return _sum
+
+        return helper(nestedList, 1)

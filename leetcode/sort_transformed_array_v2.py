d1a6d542b72dc122578e7d6fbf661641a4338a54
Mingyu Liu <mliu@quantcast.com>
2017-01-01 23:12:25 -0800

    Sort Transformed Array - Version 2

---
+++ leetcode/sort_transformed_array_v2.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortTransformedArray(self, nums, a, b, c):
+        """
+        :type nums: List[int]
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: List[int]
+        """
+        return sorted(map(lambda x:a * x**2 + b * x + c, nums))

dc80d34617fc1ce88ccc080f657535e8f37d0142
Mingyu Liu <mliu@quantcast.com>
2017-01-22 10:39:05 -0800

    Hilbert Curve

---
+++ leetcode/hilbert_curve.py
@@ -0,0 +1,23 @@
+def hilbertCurve(x, y, iter):
+    if iter == 0:
+        return 1
+
+    len = 1 << (iter - 1)
+    num = 1 << (2 * (iter - 1))
+
+    if x >= len and y >= len:
+        # 3 Shape is identical with previous iteration
+        return 2 * num + hilbertCurve(x - len, y - len, iter - 1)
+    elif x < len and y >= len:
+        # 2 Shape is identical with previous iteration
+        return num + hilbertCurve(x, y - len, iter - 1)
+    elif x < len and y < len:
+        # 1 Clock-wise rotate 90
+        return hilbertCurve(y, x, iter - 1)
+    else:
+        # 4 Anti-Clockwise rotate 90
+        return 3 * num + hilbertCurve(len - 1 - y, 2 * len - 1 - x, iter - 1)
+
+print hilbertCurve(1, 1, 2) # 3
+print hilbertCurve(0, 1, 1) # 2
+print hilbertCurve(2, 2, 2) # 9

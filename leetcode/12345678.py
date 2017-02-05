b4d3f6669676183269ce1634002a3deb835fb115
Mingyu Liu <mliu@quantcast.com>
2017-02-04 21:55:04 -0800

    123456789 - Target Sum

---
+++ leetcode/12345678.py
@@ -0,0 +1,11 @@
+def step(s1, i):
+    if i == 10:
+        if eval(s1) == 100:
+            print s1
+        return
+    if i != 1:
+        step(s1 + '+' + str(i), i + 1)
+    step(s1 + '-' + str(i), i + 1)
+    step(s1 + str(i), i + 1)
+
+step('', 1)
